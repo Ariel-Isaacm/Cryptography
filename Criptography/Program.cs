77de19dd06bf5631b3ccbd9bb40b2b89112622a5
Ariel Isaac <ariel.isaacm@gmail.com>
2015-05-21 18:14:20 +0200

    Algoritmo vernam

--- Criptography/Program.cs
+++ Criptography/Program.cs
@@ -10,36 +10,41 @@ namespace Criptography
     {
         static void Main(string[] args)
         {
-            //constructor que acepta la frase a cifrar y el valor de p
-            Cesar cesar = new Cesar("", 9);
-            String reverse = "UJCRE NAXV MWJ NAXV NVXLNK UURF HQYJAPXCYHAL BTAXFCNW ANCDYVXL XC NEXV XC NDWRCWXL BWXRCJLRWDVVXL MWJ NLANVVXL BJ NADCDO NQC WR MWJ";
-            String straight = "";
-            int x = 1;
-            for (int i = reverse.Length; i > 0; i--)
-            {
+            ////constructor que acepta la frase a cifrar y el valor de p
+            //Cesar cesar = new Cesar("", 9);
+            //String reverse = "UJCRE NAXV MWJ NAXV NVXLNK UURF HQYJAPXCYHAL BTAXFCNW ANCDYVXL XC NEXV XC NDWRCWXL BWXRCJLRWDVVXL MWJ NLANVVXL BJ NADCDO NQC WR MWJ";
+            //String straight = "";
+            //int x = 1;
+            //for (int i = reverse.Length; i > 0; i--)
+            //{
 
-                straight += reverse[reverse.Length -x];
-                x++;
-            }
-            cesar.decode(straight);
-            Console.WriteLine(cesar.getdecodedPhrase());
+            //    straight += reverse[reverse.Length -x];
+            //    x++;
+            //}
+            //cesar.decode(straight);
+            //Console.WriteLine(cesar.getdecodedPhrase());
             
             
             
-            cesar.encode();
-            cesar.decode("L JRW LW");
-            Console.WriteLine("Algoritmo cesar");
-            Console.WriteLine("Palabra cifrada");
-            //HELLO EVERYBODY
-            //H SLJABYL PU LUNSPZO
+            //cesar.encode();
+            //cesar.decode("L JRW LW");
+            //Console.WriteLine("Algoritmo cesar");
+            //Console.WriteLine("Palabra cifrada");
+            ////HELLO EVERYBODY
+            ////H SLJABYL PU LUNSPZO
             
-            Console.WriteLine("Palabra decifrada");
-            Console.WriteLine(cesar.getdecodedPhrase());
+            //Console.WriteLine("Palabra decifrada");
+            //Console.WriteLine(cesar.getdecodedPhrase());
 
-            Console.WriteLine("Sustitución");
-            Sustitucion sus = new Sustitucion("ynficwlbkuomxsevzpdrjgthaq", "hola");
-            Console.WriteLine(sus.translate());
-            Console.ReadKey();
+            //Console.WriteLine("Sustitución");
+            //Sustitucion sus = new Sustitucion("ynficwlbkuomxsevzpdrjgthaq", "hola");
+            //Console.WriteLine(sus.translate());
+            //Console.ReadKey();
+
+            Vernam v = new Vernam("Hola");
+            Console.WriteLine("El mensaje hola cifrado es..."+v.cifrar());
+            Console.WriteLine("El mensaje descifrad    es..." + v.descifrar());
+            Console.Read();
         }
 
 
---
+++ Criptography/Vernam.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criptography
+{
+    class Vernam
+    {
+        String cifrado;
+        String msj;
+        ArrayList numeros;
+        Random random;
+        int randomNumber;
+        String descifrado;
+        public Vernam(String mensaje)
+        {
+            this.msj = mensaje;
+            random = new Random(); 
+        }
+        public string cifrar()
+        {
+            
+            numeros = new ArrayList();
+
+            for (int i = 0; i < msj.Length; i++)
+            {
+              randomNumber = random.Next(0, 100);
+              numeros.Add(randomNumber);
+              cifrado += (char)((int)msj[i] + randomNumber);
+            }
+            return cifrado;
+        }
+        public string descifrar()
+        {
+            descifrado = null;
+            for (int i = 0; i < cifrado.Length; i++)
+            {
+                descifrado+=(char)((int)cifrado[i]-(int)numeros[i]);
+            }
+            return descifrado;
+        }
+    }
+}
